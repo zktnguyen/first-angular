2acb91a1177dced29a35ae0cd5261bf74b339f75
Kim Nguyen <zktnguyen@ucdavis.edu>
2016-11-24 21:06:19 -0800

    First commit

---
+++ app/view1/view1.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('myApp.view1', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view1', {
+    templateUrl: 'view1/view1.html',
+    controller: 'View1Ctrl'
+  });
+}])
+
+.controller('View1Ctrl', ['$scope', function($scope) {
+  $scope.name = '';
+  $scope.names=[{name:"Kim"}, {name:"Cindy"}];
+  $scope.addName = function(){
+    $scope.names.push( {'name': $scope.name});
+    $scope.name = '';
+  };
+
+}]);
